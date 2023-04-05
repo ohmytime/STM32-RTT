26d134ddba84c6d932c7db2c42ba6430aca65317
kevin <lipengfei.dev@gmail.com>
2023-04-05 20:07:46 +0800

    Update main.c
    增加延时

--- src/main.c
+++ src/main.c
@@ -9,8 +9,9 @@ int main(void)
     {
         cnt++;
         LOGI("This is Info");
-        LOGE("如果想显示中文，请使用RTT Client !");
+        LOGW("如果想显示中文，请使用RTT Client !");
         LOGE("This is an error info: %d", cnt);
+        HAL_Delay(1000);
     }
 
     return 0;
