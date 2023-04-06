94e5ec3e77fdabc3ae30a1dcfbf70edbd5a2588c
kevin <lipengfei.dev@gmail.com>
2023-04-06 13:39:09 +0800

    优化例子

--- src/main.c
+++ src/main.c
@@ -1,17 +1,32 @@
-#include "stm32f1xx_hal.h"
-#include "rttlogger.h"
+#include "main.h"
 
+void Delay(uint16_t n)
+{
+    while (n)
+    {
+        n--;
+    }
+    
+}
 int main(void)
 {
-    
+
+
     int cnt = 0;
+    float fCnt = 3.1415926;
+    LOG_CLEAR();
+
     while (1)
     {
-        cnt++;
         LOGI("This is Info");
         LOGW("如果想显示中文,请使用RTT Client !");
-        LOGE("Value: %d, 0b%b", cnt,cnt);
-        HAL_Delay(1000);
+        LOGE("显示8位二进制: %d, %b", cnt, cnt);
+        LOGE("显示全部32位的二进制,%d -> %B", cnt, cnt);
+        LOGE("显示浮点数(小数点后三位) %f",fCnt);
+
+        cnt++;
+        fCnt++;
+        Delay(3000);
     }
 
     return 0;
